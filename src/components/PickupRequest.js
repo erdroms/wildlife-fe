84ef40d5da39d9ad8bcdb77cd42da79c5139205f
Erin Droms <erin.droms@rtslabs.com>
2019-10-20 17:55:24 -0400

    get pickup requests from api

--- src/components/PickupRequest.js
+++ src/components/PickupRequest.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import {Button} from './common';
+import { Button } from "./common";
 
-
-const PickupRequest = ({details}) => {
-  const { location, description } = details;
+const PickupRequest = ({ details }) => {
+  const {
+    address1,
+    address2,
+    // createdAt,
+    // dropoffLocation,
+    // firstName,
+    // lastName,
+    pickupDescription,
+    // pickupId,
+    pickupType,
+    // userId,
+    // volunteers,
+  } = details;
 
   return (
     <div className="pickup-request">
-      <p className="mb-0"><b>Location:</b> {location}</p>
-      <p><b>Description:</b> {description}</p>
+      <p className="mb-0">
+        <b>Location:</b> {address1}, {address2}
+      </p>
+      <p className="mb-0">
+        <b>Type:</b> <span className="text-capitalize">{pickupType}</span>
+      </p>
+      <p>
+        <b>Description:</b> {pickupDescription}
+      </p>
       <div className="d-flex align-items-center">
-        <Button buttonClass="primary" text="Initiate Pickup" className="mr-2 w-100" />
-        <Button buttonClass="secondary" text="View Details" className="w-100" />
+        <Button buttonClass="primary" className="mr-2 w-100">
+          Initiate Pickup
+        </Button>
+        <Button buttonClass="secondary" className="w-100">
+          View Details
+        </Button>
       </div>
     </div>
-  )
+  );
 };
 
 export default PickupRequest;
--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
 
-import { Button, ButtonLink } from '../common';
-import PickupRequest from '../PickupRequest';
-import ActivePickup from '../ActivePickup';
+import { ButtonLink } from "../common";
+import PickupRequest from "../PickupRequest";
+import ActivePickup from "../ActivePickup";
 
-const Dashboard = (props) => (
-  <div className="container">
-    <div className="row">
-      <div className="col text-center">
-        <ButtonLink to="/pickup-request" buttonClass="primary" text="Add new pickup request" className="w-100" />
+const Dashboard = (props) => {
+  const [pickupRequests, setPickupRequests] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    // Get the list of pickup requests, and update it when page changes
+    API.get("wildlife-admin", "/pickups").then((res) => setPickupRequests(res));
+  }, [page]);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center">
+          <ButtonLink to="/pickup-request" buttonClass="primary" text="Add new pickup request" className="w-100" />
+        </div>
+      </div>
+      <div className="mt-4">
+        <h6>Pickup Requests:</h6>
+      </div>
+      <div className="row">
+        <div className="col">
+          {pickupRequests.map((request) => (
+            <PickupRequest key={request.pickupId} details={request} />
+          ))}
+        </div>
+      </div>
+      <div className="mt-4">
+        <h6>Active Pickups:</h6>
+      </div>
+      <div className="row">
+        <div className="col">
+          <ActivePickup details={{ location: "123 N Wild St, Richmond, VA 23015", description: "Injured Owl" }} />
+          <ActivePickup details={{ location: "345 Wilderness Ave, Staunton, VA 24401", description: "Baby Deer" }} />
+        </div>
       </div>
     </div>
-    <div className="mt-4">
-      <h6>Pickup Requests:</h6>
-    </div>
-    <div className="row">
-      <div className="col">
-        <PickupRequest details={{location: "123 N Wild St, Richmond, VA 23015", description: "Injured Owl"}} />
-        <PickupRequest details={{location: "345 Wilderness Ave, Staunton, VA 24401", description: "Baby Deer"}} />
-      </div>
-    </div>
-    <div className="mt-4">
-      <h6>Active Pickups:</h6>
-    </div>
-    <div className="row">
-      <div className="col">
-        <ActivePickup details={{location: "123 N Wild St, Richmond, VA 23015", description: "Injured Owl"}} />
-        <ActivePickup details={{location: "345 Wilderness Ave, Staunton, VA 24401", description: "Baby Deer"}} />
-      </div>
-    </div>
-  </div>
-)
+  );
+};
 
 export default Dashboard;
